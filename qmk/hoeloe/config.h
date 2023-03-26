d3304172fd00e86b3979f61705aa3dd4af3edc0b
rrirrirr <drpihl@gmail.com>
2023-03-26 12:46:14 +0200

    first commit

---
+++ qmk/hoeloe/config.h
@@ -0,0 +1,43 @@
+/* Copyright 2022 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#define TAPPING_TERM 200
+
+#pragma once
+
+#define MASTER_RIGHT
+// #define EE_HANDS
+#define SPLIT_POINTING_ENABLE
+#define POINTING_DEVICE_RIGHT
+
+// #define POINTING_DEVICE_GESTURES_ENABLE
+#define POINTING_DEVICE_CURSOR_GLIDE_ENABLE
+#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
+
+// #define CIRQUE_PINNACLE_POSITION_MODE = CIRQUE_PINNACLE_RELATIVE_MODE
+// #define CIRQUE_PINNACLE_POSITION_MODE = yes
+// #define CIRQUE_PINNACLE_POSITION_MODE = CIRQUE_PINNACLE_ABSOLUTE_MODE
+#define CIRQUE_PINNACLE_TAP_ENABLE
+#define CIRQUE_PINNACLE_SIDE_SCROLL_ENABLE
+// #define CIRQUE_PINNACLE_CURVED_OVERLAY 
+// #define CIRQUE_PINNACLE_CURVED_OVERLAY 
+#define CIRQUE_PINNACLE_DIAMETER_MM 35
+// #define CIRQUE_PINNACLE_DIAMETER_MM 40
+
+
+
+
+// Lets you roll mod-tap keys
+#define IGNORE_MOD_TAP_INTERRUPT
